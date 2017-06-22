33e80206777fd155c55556418633db4df90e644f
donalbertico <atates@udlanet.ec>
2017-06-22 02:26:22 -0500

    fiin typo

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -1,10 +1,8 @@
 var request = require('request');
 module.exports = {
   search : function(req,res,nex){
-    console.log(req.body.street);
-    console.log(req.param('street'));
     var street = req.body.street;
-    request.get({ url : 'https://integration-controll.herokuapp.com/control/search?s='+street},function(err,response,control){
+    request.get({ url : 'https://integration-controll.herokuapp.com/controls/search?s='+street},function(err,response,control){
       console.log('primera respuesta',control);
       request.get({url : 'https://integration-trafic.herokuapp.com/trafic/search?s='+street},function(err,response,trafic){
         console.log('segunda respuesta',trafic);
